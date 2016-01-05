59505065a571ea49f251a0cf114e82610447b79a
McJty <mcjty1@gmail.com>
2016-01-05 09:32:09 +0100

    Updated forge and added waila dependency

--- src/main/java/mcjty/gearswap/GearSwap.java
+++ src/main/java/mcjty/gearswap/GearSwap.java
@@ -17,7 +17,7 @@ import java.io.File;
         version = GearSwap.VERSION)
 public class GearSwap {
     public static final String MODID = "gearswap";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
     public static final String MIN_FORGE_VER = "11.15.0.1600";
 
     @SidedProxy(clientSide="mcjty.gearswap.proxy.ClientProxy", serverSide="mcjty.gearswap.proxy.ServerProxy")
