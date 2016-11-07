1b91a477dc29795825898c9164abec6f2cbdd187
McJty <mcjty1@gmail.com>
2016-11-07 10:08:12 +0100

    Updated Baubles API

--- src/api/java/baubles/api/package-info.java
+++ src/api/java/baubles/api/package-info.java
@@ -1,4 +1,5 @@
-@API(owner = "Baubles", apiVersion = "1.0.1.10", provides = "Baubles|API")
+@API(owner = "Baubles", apiVersion = "1.3.1.0", provides = "Baubles|API")
 package baubles.api;
 
 import net.minecraftforge.fml.common.API;
+
