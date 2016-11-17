e7e1ef6a3727741434aa91d47afe66dcb29fca7f
McJty <mcjty1@gmail.com>
2016-11-17 12:43:00 +0100

    1.3.0alpha

--- src/main/java/mcjty/gearswap/GearSwap.java
+++ src/main/java/mcjty/gearswap/GearSwap.java
@@ -20,10 +20,11 @@ import java.io.File;
 
 @Mod(modid = GearSwap.MODID, name="Gear Swapper",
 //        dependencies = "required-after:Forge@["+ GearSwap.MIN_FORGE_VER+",)",
+        acceptedMinecraftVersions = "[1.9,1.12)",
         version = GearSwap.VERSION)
 public class GearSwap {
     public static final String MODID = "gearswap";
-    public static final String VERSION = "1.2.7";
+    public static final String VERSION = "1.3.0alpha";
     public static final String MIN_FORGE_VER = "11.15.0.1600";
 
     @SidedProxy(clientSide="mcjty.gearswap.proxy.ClientProxy", serverSide="mcjty.gearswap.proxy.ServerProxy")
