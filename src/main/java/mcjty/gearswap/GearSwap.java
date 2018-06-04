d8d0bb43dccf99ff02b246d468af02d98ed6993b
McJty <mcjty1@gmail.com>
2018-06-04 17:45:04 +0200

    Updated versions

--- src/main/java/mcjty/gearswap/GearSwap.java
+++ src/main/java/mcjty/gearswap/GearSwap.java
@@ -24,7 +24,7 @@ import java.io.File;
         version = GearSwap.VERSION)
 public class GearSwap implements ModBase {
     public static final String MODID = "gearswap";
-    public static final String VERSION = "1.6.0-alpha";
+    public static final String VERSION = "1.6.0";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_MCJTYLIB_VER = "3.0.0";
 
