32a1fd241f6497170c48be3bd476a82e068d3a89
McJty <mcjty1@gmail.com>
2019-03-11 11:41:15 +0100

    More cleanups and removal of unneeded things

--- src/main/java/mcjty/gearswap/setup/ClientProxy.java
+++ src/main/java/mcjty/gearswap/setup/ClientProxy.java
@@ -1,26 +1,7 @@
 package mcjty.gearswap.setup;
 
-import mcjty.lib.McJtyLibClient;
 import mcjty.lib.setup.DefaultClientProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+@SuppressWarnings("EmptyClass")
 public class ClientProxy extends DefaultClientProxy {
-
-    @Override
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
-        McJtyLibClient.preInit(e);
-    }
-
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
-    }
 }
